5c3061c044544081aa0f9e509a72c9b30dcd0572
Aljaž Ostrež <aljaz.ostrez@gmail.com>
2020-05-25 10:51:28 +0200

    komentar za datum

--- aplikacija.py
+++ aplikacija.py
@@ -404,9 +404,9 @@ def liga(liga,krog):
         (liga)
     )
     uporabniki = cur.fetchall()
-    # datum = date.today()
+    datum = date.today()
     # datum = date(2020,3,13)
-    datum = date(2020,3,14)
+    # datum = date(2020,3,14)
     return template(
         "predloge/rezultati.html",
         tekme=tekme,
